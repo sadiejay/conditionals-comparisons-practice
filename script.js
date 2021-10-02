d2b83356404d89adfb79cf6919137f480307f978
Sadie <19538219+sadiejay@users.noreply.github.com>
2021-10-02 10:55:33 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// ex 1
+// var number = 12;
+// var string = "12";
+
+// if (number === string) {
+//   console.log("true");
+// } else if (number == string) {
+//   console.log("true");
+// } else {
+//   console.log("nothing is true");
+// }
+
+// ex 2
+// var sleep = Number(
+//   prompt(
+//     "Enter a whole number (0 – 12) representing their hours of sleep"
+//   )
+// );
+
+// if (sleep > 12) {
+//   alert("be truthful and try again. :)");
+// } else if (sleep > 8) {
+//   console.log("Congratulations!");
+// } else if (sleep > 5) {
+//   console.log(
+//     "You did ok, consider getting more sleep."
+//   );
+// } else {
+//   console.log("GET MORE SLEEP!");
+// }
+
+// EX 3
+// var choco = Number(
+//   prompt(
+//     "how many pieces of chocolate would you like? (0-10) whole numbers only"
+//   )
+// );
+
+// var leftOver = 10 - choco;
+
+// if (leftOver >= 7) {
+//   console.log(
+//     `There's ${leftOver} pieces left. Take a couple to friends!`
+//   );
+// } else if (leftOver > 4) {
+//   console.log(
+//     `There's just enough left over for me too! Thanks!`
+//   );
+// } else {
+//   console.log(`You needed a snack! Enjoy :D`);
+// }
